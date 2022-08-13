c879326715e3f1afb9f7d6c56a4e3f6bc58bfe26
hxrd <tibcso0322@gmail.com>
2022-08-13 18:49:00 +0200

    Now strict mode is controllable

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.js'
 import { MantineProvider } from '@mantine/core'
 
+const strict = true
+
+console.log(`Strict: ${strict}`)
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <>
+    { strict ? 
     <React.StrictMode>
       <MantineProvider>
         <App />
       </MantineProvider>
     </React.StrictMode>
+    : <>
+    <MantineProvider>
+        <App />
+    </MantineProvider></>}
   </>
 )

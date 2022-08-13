507824dd857a3b1c4507750fb3f0819d4b991a21
hxrd <tibcso0322@gmail.com>
2022-08-13 14:28:41 +0200

    Refactor

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.js'
 import { MantineProvider } from '@mantine/core'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <MantineProvider>
-      <App />
-    </MantineProvider>
-  </React.StrictMode>
+  <>
+    <React.StrictMode>
+      <MantineProvider>
+        <App />
+      </MantineProvider>
+    </React.StrictMode>
+  </>
 )

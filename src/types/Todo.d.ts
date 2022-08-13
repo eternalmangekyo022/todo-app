aa144c626d2f698aa3bde37aac1c8b0c6dc8aba6
hxrd <tibcso0322@gmail.com>
2022-08-13 14:27:22 +0200

    Added Todo.d.ts

---
+++ src/types/Todo.d.ts
@@ -0,0 +1,29 @@
+declare module './components/Todo'
+
+export interface Todo {
+    text: string
+    id: number
+    complete: boolean
+}
+
+export interface TodoProps extends Todo {
+    dispatch: React.Dispatch<TodoAction>
+}
+
+export type TodoAction =
+{
+    type: "add"
+    content: Todo
+} | 
+{
+    type: 'remove'
+    id: number
+} | 
+{
+    type: "empty"
+} | 
+{
+    type: "update"
+    id: number
+    complete: boolean
+}

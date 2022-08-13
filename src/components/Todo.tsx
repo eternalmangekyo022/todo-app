5a5e877e2468041868092d51d1c1bb006a6c44b5
Tibor Fazekas <89131915+eternalmangekyo022@users.noreply.github.com>
2022-08-13 20:14:09 +0200

    Updates to button methodology

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -14,7 +14,7 @@ function Todo({ dispatch, id, complete, text, last }: TodoProps): JSX.Element {
     return <>
         <div key={id} style={{ width: '20rem' }}>
             <Stack justify='center' align='center' style={{ flexDirection: 'row', position: 'relative' }}>
-{/* 20% */}     <Button disabled={complete} style={{ width: '20%', left: '1rem', position: 'absolute' }} onClick={deleteTodo}><IconTrash /></Button>
+{/* 20% */}     <Button style={{ width: '20%', left: '1rem', position: 'absolute' }} onClick={deleteTodo}><IconTrash /></Button>
 {/* 60% */}     <Container style={{ width: '60%', display: 'flex', justifyContent: 'center' }}>
                     <Button
                         variant={complete ? 'filled' : 'gradient'}
@@ -25,7 +25,7 @@ function Todo({ dispatch, id, complete, text, last }: TodoProps): JSX.Element {
                         {text.length > 12 ? text.slice(0, 12) + '...' : text}
                     </Button>
                 </Container>
-{/* 20% */}     <Button style={{ width: '20%', right: '1rem', position: 'absolute' }} onClick={() => dispatch({ type: "update", id, complete: !complete })}>{complete ? <IconX/> : <IconCheck/>}</Button>
+{/* 20% */}     <Button color={ complete ? "red" : "green"} style={{ width: '20%', right: '1rem', position: 'absolute' }} onClick={() => dispatch({ type: "update", id, complete: !complete })}>{complete ? <IconX/> : <IconCheck/>}</Button>
             </Stack>
             <Modal style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} centered withCloseButton={true} opened={active} onClose={activeWrapper(false)} >
                 <Center>

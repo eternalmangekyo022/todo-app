de1c019a46dc90a224906984e72234586c8fe87e
hxrd <tibcso0322@gmail.com>
2022-08-13 14:30:27 +0200

    Memoized the component,  removed insignificant comments

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,16 +1,11 @@
 import { Button, Modal, Stack } from "@mantine/core";
-import { useState } from "react";
-import { TodoProps } from "../types.js";
+import React, { useState } from "react";
+import { TodoProps } from "../types/Todo.js";
 
-export default function Todo({ dispatch, id, complete, text }: TodoProps): JSX.Element {
-    /* const [complete, setComplete] = useState<boolean>(false); */
-
+function Todo({ dispatch, id, complete, text }: TodoProps): JSX.Element {
     const [active, setActive] = useState<boolean>(false);
     const wrapper: (val: boolean) => (() => void) = (val: boolean): (() => void) => { return () => { setActive(val) } }
-    // const untoggled: React.CSSProperties = { minHeight: '3rem', maxHeight: '6rem', maxWidth: '12rem' };
-    // const toggledState: React.CSSProperties = { height: '100%', width: '100%', position: 'absolute', zIndex: 1 };
 
-    // const [style, toggleStyle] = useToggleObject<React.CSSProperties, React.CSSProperties>([untoggled, toggledState]);
 
     return <>
         <div key={id}>
@@ -20,25 +15,20 @@ export default function Todo({ dispatch, id, complete, text }: TodoProps): JSX.E
             style={{ margin: '1rem', minHeight: '3rem', maxHeight: '6rem', maxWidth: '12rem' }}
             onClick={wrapper(true)}
         >
-            {/* minus icon here */}{text.length > 12 ? text.slice(0, 12) + '...' : text}{/* check icon here */}
+            {/* minus icon here? */}{text.length > 12 ? text.slice(0, 12) + '...' : text}{/* check icon here? */}
         </Button>
-        <Modal centered withCloseButton={true} opened={active} onClose={wrapper(false)}>
-            { active ?
-            <>
-
-                <Button onClick={() => dispatch({ type: 'remove', id: id })}>Delete</Button>
-                <Button
-                    onClick={() => dispatch({ type: 'update', id: id, complete: !complete })}
-                >{complete ? "Not complete" : "Complete task"}</Button>
-
-            </> : <>
-                
+        <Modal centered withCloseButton={true} opened={active} onClose={wrapper(false)} >
+            <Stack align='center' justify='space-around'>
+                <>
+                    <Button onClick={() => {dispatch({ type: 'remove', id: id }); setActive(false)}}>Delete</Button>
+                    <Button
+                        onClick={() => dispatch({ type: 'update', id: id, complete: !complete })}
+                    >{complete ? "Not complete" : "Complete task"}</Button>
                 </>
-
-            }
-            <Stack justify="center">
             </Stack>
         </Modal>
         </div>
     </>;
 }
+
+export default React.memo(Todo)
